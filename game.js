86ffbd6d04237b5730a6c19dfd483002d8ddff0b
Joshua Widjaja <josh@widjaja.us>
2023-05-12 16:22:36 -0700

    Same commit as previous

--- game.js
+++ game.js
@@ -1,4 +1,32 @@
+class Page1 extends Phaser.Scene {
+
+}
+
+class Page2 extends Phaser.Scene {
+
+}
+
+class Page3 extends Phaser.Scene {
+
+}
+
+class Stage1 extends PhysicsMap {
+
+}
+
+class Stage2 extends PhysicsMap {
+
+}
+
+class Stage3 extends PhysicsMap {
+
+}
+
 const game = new Phaser.Game({
     width: 800,
     height: 600,
+    physics: {
+        default: 'matter',
+    },
+    scene: [Page1, Stage1, Page2, Stage2, Page3, Stage3],
 });
