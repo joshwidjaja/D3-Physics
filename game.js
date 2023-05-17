8574f12516e969b0f1a14695767382bdadbb9143
Joshua Widjaja <josh@widjaja.us>
2023-05-16 20:17:23 -0700

    scene 3 setup, testing collision

--- game.js
+++ game.js
@@ -34,22 +34,60 @@ class Stage2 extends PhysicsMap {
     }
 
     onEnter() {
-        this.physicsContainer.setOnCollide(() => {
-            this.scene.start('page3');
-        });
-
         this.musha = this.matter.add.image(200, 100, 'musha', null)
             .setScale(1)
             .setBounce(0.4)
-            .setCollisionCategory(this.noDrag)
+            .setCollisionGroup(this.noDrag)
+            .setCollisionCategory(this.object)
 
-        this.physicsContainer.setCollidesWith(this.noDrag);
+        this.physicsContainer.setCollidesWith(this.object);
+
+        this.physicsContainer.setOnCollide(() => {
+            this.scene.start('page3');
+        });
     }
 }
 
 class Stage3 extends PhysicsMap {
     constructor() {
         super("stage3");
+    }
+
+    onEnter() {
+        let banana = this.matter.add.image(200, 200, 'banana', null)
+            .setScale(0.1)
+            .setStatic(true)
+
+        this.musha = this.matter.add.image(50, 100, 'musha', null)
+            .setScale(1)
+            .setBounce(0.4)
+            .setCollisionGroup(this.noDrag)
+            .setCollisionCategory(this.object)
+
+        this.physicsContainer.setCollidesWith(this.object);
+
+        this.physicsContainer.setOnCollide(() => {
+            this.scene.start('end');
+        })
+        
+        this.wall = this.matter.add.gameObject(this.rect)
+            .setStatic(true)
+            .setCollidesWith([this.player, this.object])
+            .setOnCollide(() => {
+                //this.scene.start('scene3');
+                this.soysauce.setX(50);
+                this.soysauce.setY(200);
+
+                this.musha.setX(50);
+                this.musha.setY(100);
+            })
+            
+    }
+}
+
+class End extends Phaser.Scene {
+    constructor() {
+        super("end")
     }
 }
 
@@ -67,5 +105,5 @@ const game = new Phaser.Game({
             debugBodyColor: 0xffffff,
         },
     },
-    scene: [Stage2, Page1, Stage1, Page2, Page3, Stage3],
+    scene: [Stage3, Page1, Stage1, Page2, Stage2, Page3],
 });
